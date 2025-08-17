1890c39a31ce4c6eb6674b12d0d8e53ad39f7ceb
= <=>
2025-08-17 11:54:45 +0000

    add new pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,43 @@
-import { Container } from '@mui/material';
-import { TasksPage } from '../pages/TasksPage';
+// App.tsx
+import { Container } from "@mui/material";
+import { BrowserRouter as Router, useRoutes, Navigate, Outlet } from "react-router-dom";
+import { TasksPage } from "../pages/TasksPage";
+
+const UsersPage = () => (
+  <div>
+    <h2>Главная страница пользователей</h2>
+    <Outlet />
+  </div>
+);
+const UsersListPage = () => <div>Список пользователей</div>;
+const UserDetailPage = () => <div>Детали пользователя</div>;
+
+const routes = [
+  { path: "/", element: <Navigate to="/tasks" replace /> },
+  { path: "/tasks", element: <TasksPage /> },
+  {
+    path: "/users",
+    element: <UsersPage />,
+    children: [
+      { path: "/users/list", element: <UsersListPage /> },
+      { path: "/users/:id", element: <UserDetailPage /> },
+    ]
+  },
+];
+
+function AppRoutes() {
+  const element = useRoutes(routes);
+  return element;
+}
 
 function App() {
-
   return (
-    <Container><TasksPage /></Container>
+    <Router>
+      <Container sx={{ mt: 2 }}>
+        <AppRoutes />
+      </Container>
+    </Router>
   );
 }
 
-export default App
+export default App;
